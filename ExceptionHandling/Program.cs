7d31a717da3d44f71e07ce8fc29378f4f6c2b758
GreedNeSS <ruslan199126@gmail.com>
2022-05-19 21:30:24 +0300

    Exception handling.

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,6 +1,42 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+// #1
+//app.UseDeveloperExceptionPage();
 
-app.MapGet("/", () => "Hello World!");
+//#2
+//app.Environment.EnvironmentName = "Production";
+
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/error");
+//}
+
+//app.Map("/error", app =>
+//{
+//    app.Run(async context =>
+//    {
+//        context.Response.StatusCode = 500;
+//        await context.Response.WriteAsync("Error Page");
+//    });
+//});
+
+//#3
+app.Environment.EnvironmentName = "Production";
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler(app => app.Run(async context =>
+    {
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsync("Error Page");
+    }));
+}
+
+app.Run(async context =>
+{
+    int a = 5;
+    int b = 0;
+    await context.Response.WriteAsync($"a / b = {a/b}");
+});
 
 app.Run();
